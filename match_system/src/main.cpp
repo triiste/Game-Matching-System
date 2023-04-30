93abd22f83ae0ba5122191c7f98bd31373e5fd6c
wcf <1256933608@qq.com>
2023-04-30 12:56:17 +0800

    match server:4.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -4,8 +4,12 @@
 #include "match_server/Match.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/ThreadFactory.h>
 #include <thrift/transport/TTransportUtils.h>
 #include "save_client/Save.h"
+#include <thrift/TToString.h>
+#include <thrift/server/TThreadedServer.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
@@ -80,7 +84,7 @@ class Pool
         void match(){
             while(users.size() > 1){
                 sort(users.begin(),users.end(),[&](User &a,User b){
-                    return a.score<b.score;
+                        return a.score<b.score;
                         });
                 bool flag = true;
                 for(uint32_t i = 1; i<users.size();i++)
@@ -132,6 +136,23 @@ class MatchHandler : virtual public MatchIf {
         }
 
 };
+class MatchCloneFactory : virtual public MatchIfFactory {
+    public:
+        ~MatchCloneFactory() override = default;
+        MatchIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo) override
+        {
+            std::shared_ptr<TSocket> sock = std::dynamic_pointer_cast<TSocket>(connInfo.transport);
+/*            cout << "Incoming connection\n";
+            cout << "\tSocketInfo: "  << sock->getSocketInfo() << "\n";
+            cout << "\tPeerHost: "    << sock->getPeerHost() << "\n";
+            cout << "\tPeerAddress: " << sock->getPeerAddress() << "\n";
+            cout << "\tPeerPort: "    << sock->getPeerPort() << "\n";*/
+            return new MatchHandler;
+        }
+        void releaseHandler(MatchIf* handler) override {
+            delete handler;
+        }
+};
 //生产者-消费者模型
 void consume_task(){
     //对队列操作是共享的，操作完队列就解锁
@@ -162,14 +183,14 @@ void consume_task(){
 }
 
 int main(int argc, char **argv) {
-    int port = 9090;
-    ::std::shared_ptr<MatchHandler> handler(new MatchHandler());
-    ::std::shared_ptr<TProcessor> processor(new MatchProcessor(handler));
-    ::std::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-    ::std::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-    ::std::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
-    TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+    TThreadedServer server(
+            std::make_shared<MatchProcessorFactory>(std::make_shared<MatchCloneFactory>()),
+            std::make_shared<TServerSocket>(9090), //port
+            std::make_shared<TBufferedTransportFactory>(),
+            std::make_shared<TBinaryProtocolFactory>());
+    
+
     cout<<"Start Match Server"<<endl;
     //多线程 一堆消费者的线程和一堆生产者的线程
     //生产者和消费者之间需要通信的媒介
